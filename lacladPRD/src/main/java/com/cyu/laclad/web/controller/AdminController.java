b858ebd86e5d6bdc81e5d02d063369030fb32f34
Alvaro GFT <ajcr81@gmail.com>
2016-11-25 02:35:54 -0600

    Role based security Configuration

--- lacladPRD/src/main/java/com/cyu/laclad/web/controller/AdminController.java
+++ lacladPRD/src/main/java/com/cyu/laclad/web/controller/AdminController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -25,6 +26,7 @@ import com.cyu.laclad.enums.Status;
 import com.cyu.laclad.utils.LacladUtils;
 import com.cyu.laclad.web.command.AdminCommand;
 
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/admins")
 @Controller
 @RooWebScaffold(path = "admins", formBackingObject = Admin.class)
